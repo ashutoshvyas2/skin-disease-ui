d0369e7110dcb1c56df0ced78cbb5b31263a42a3
ashutoshvyas2 <146797030+ashutoshvyas2@users.noreply.github.com>
2026-05-08 10:15:35 +0530

    add dockerfile for huggingface

--- app.py
+++ app.py
@@ -103,4 +103,5 @@ def predict():
 
 if __name__ == '__main__':
     print("🌍 Server starting at http://127.0.0.1:5000")
-    app.run(host='127.0.0.1', port=5000, debug=False)
+    port = int(os.environ.get('PORT', 7860))
+    app.run(host='0.0.0.0', port=port, debug=False)
